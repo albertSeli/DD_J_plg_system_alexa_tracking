90d472b567cbc2c03b8f0a662d8a3133075f852b
Florian Häusler <florian@didldu.com>
2017-02-16 20:03:33 +0100

    Version update

--- dd_alexa_tracking.php
+++ dd_alexa_tracking.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version    1-1-0-0 // Y-m-d 2016-05-22
+ * @version    1-2-0-1 // Y-m-d 2016-05-22
  * @author     Didldu e.K. Florian Häusler https://www.hr-it-solutions.com
  * @copyright  Copyright (C) 2011 - 2016 Didldu e.K. | HR IT-Solutions
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
